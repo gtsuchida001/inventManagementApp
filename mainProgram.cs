90429bd697f0ad8b9acad3e8f39045ea337f0f7f
Your Name <you@example.com>
2024-12-19 16:26:59 +0900

    Created a text box and added range processing

--- mainProgram.cs
+++ mainProgram.cs
@@ -1,6 +1,6 @@
 namespace inventManagementApp
 {
-    internal static class Program
+    internal static class mainProgram
     {
         /// <summary>
         ///  The main entry point for the application.
@@ -8,8 +8,6 @@ namespace inventManagementApp
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
--- obj/Debug/net8.0-windows/inventManagementApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/inventManagementApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("inventManagementApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5de3e58695acb97d2a1977ad1af41138483fbd00")]
 [assembly: System.Reflection.AssemblyProductAttribute("inventManagementApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("inventManagementApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
